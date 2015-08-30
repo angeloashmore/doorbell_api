05b8990b25b7ffbb37cc89a1b36db9411c25269f
Angelo Ashmore <angeloashmore@me.com>
2015-08-30 01:11:37 -1000

    Remove unnecessary plugs

--- lib/doorbell_api/endpoint.ex
+++ lib/doorbell_api/endpoint.ex
@@ -3,14 +3,6 @@ defmodule DoorbellApi.Endpoint do
 
   socket "/socket", DoorbellApi.UserSocket
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :doorbell_api, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
@@ -29,11 +21,6 @@ defmodule DoorbellApi.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-
-  plug Plug.Session,
-    store: :cookie,
-    key: "_doorbell_api_key",
-    signing_salt: "9YT4bTcu"
 
   plug DoorbellApi.Router
 end
